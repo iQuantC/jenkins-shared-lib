88bff466daa673cc125a6d55fdcd683f606ddebb
iQuant <iquantconsult@gmail.com>
2025-12-31 00:08:27 -0700

    Update nodePipeline.groovy

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -22,20 +22,13 @@ def call(Map params = [:]) {
                     echo 'Tests passed'
                 }
             }
-            stage('Deploy') {
+            stage('Build Docker Image') {
                 steps {
                     script {
                         // Build Docker image
                         def imageName = "my-node-app:${env.BUILD_NUMBER}"
                         sh "docker build -t ${imageName} ."
-                        
-                        // Simulate deployment: Run the container (in production, push to registry and deploy to Kubernetes/EC2)
-                        //sh "docker stop my-node-app || true"
-                        //sh "docker rm my-node-app || true"
-                        //sh "docker run -d -p 3000:3000 --name my-node-app ${imageName}"
-                        
-                        //echo 'Deployment completed. App running at http://localhost:3000'
-                        //echo 'Deployment soon'
+                        echo 'Docker Image Build Completed'
                     }
                 }
             }
